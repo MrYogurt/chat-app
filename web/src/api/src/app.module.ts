a2fd2f0571ea995168b2d55fc6ef5b6209a6c276
Egor Muravyev <egor.muravyev@bravedevelopers.com>
2021-04-19 11:58:55 +0400

    added db connection

--- web/src/api/src/app.module.ts
+++ web/src/api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 8080,
+      username: 'admin',
+      password: '228228228',
+      database: 'postgres',
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
